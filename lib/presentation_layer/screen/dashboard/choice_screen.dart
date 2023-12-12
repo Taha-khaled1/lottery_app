b23c75679c9370a2fa323d94345c0cf806a435e0
Taha khalid <tth31770@gmail.com>
2023-12-12 17:12:18 +0200

    attrbuite update

--- lib/presentation_layer/screen/dashboard/choice_screen.dart
+++ lib/presentation_layer/screen/dashboard/choice_screen.dart
@@ -1,6 +1,7 @@
 import 'package:free_lottery/presentation_layer/components/appbar.dart';
 import 'package:free_lottery/presentation_layer/components/custom_butten.dart';
 import 'package:free_lottery/presentation_layer/screen/dashboard/main_dashboard.dart';
+import 'package:free_lottery/presentation_layer/screen/dashboard/management.dart';
 import 'package:free_lottery/presentation_layer/src/style_packge.dart';
 import 'package:free_lottery/presentation_layer/utils/responsive_design/ui_components/info_widget.dart';
 import 'package:get/get.dart';
@@ -27,15 +28,15 @@ class ChoiceScreen extends StatelessWidget {
                     Get.to(() => Dashboard());
                   },
                 ),
-                // CustomButton(
-                //   width: deviceInfo.localWidth * 0.85,
-                //   height: 60,
-                //   color: ColorManager.kPrimary,
-                //   text: "Withdrawal requests",
-                //   press: () {
-                //     // Get.to(() => ManagementScreen());
-                //   },
-                // ),
+                CustomButton(
+                  width: deviceInfo.localWidth * 0.85,
+                  height: 60,
+                  color: ColorManager.kPrimary,
+                  text: "Withdrawal requests",
+                  press: () {
+                    Get.to(() => ManagementScreen());
+                  },
+                ),
               ],
             ),
           );
